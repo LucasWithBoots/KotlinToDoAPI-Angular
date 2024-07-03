6cddef159d3b7b49dac5912e452f2dd1f710244f
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-07-03 00:10:56 -0300

    feat: requisição get funcionando

--- src/app/componentes/tarefa/tarefa-estado-inicial/tarefa-estado-inicial.component.ts
+++ src/app/componentes/tarefa/tarefa-estado-inicial/tarefa-estado-inicial.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Tarefa} from "../../../tarefa.interface";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-tarefa-estado-inicial',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HttpClientModule, NgForOf],
   templateUrl: './tarefa-estado-inicial.component.html',
   styleUrl: './tarefa-estado-inicial.component.scss'
 })
-export class TarefaEstadoInicialComponent {
+export class TarefaEstadoInicialComponent implements OnInit {
+
+  tarefas: Tarefa[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.fetchDetails();
+  }
+
+  public fetchDetails() {
+    this.http.get<Tarefa[]>("http://localhost:8080/tarefa").subscribe(
+      (res: Tarefa[]) => {
+        this.tarefas = res;
+      },
+      (err) => {
+        console.error('Erro ao buscar tarefas', err);
+      }
+    );
+  }
+
 
 }
