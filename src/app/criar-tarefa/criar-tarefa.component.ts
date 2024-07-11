9cf4be3e93f47f890be9a8120bd624f63914c278
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-07-11 15:04:32 -0300

    style: prettier

--- src/app/criar-tarefa/criar-tarefa.component.ts
+++ src/app/criar-tarefa/criar-tarefa.component.ts
@@ -1,16 +1,13 @@
-import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-tarefa',
   standalone: true,
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   templateUrl: './criar-tarefa.component.html',
   styleUrl: './criar-tarefa.component.scss',
 })
 export class CriarTarefaComponent {
   enteredTask: string = 'Escreva aqui...';
-
 }
